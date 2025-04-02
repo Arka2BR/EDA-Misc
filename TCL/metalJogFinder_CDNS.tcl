b04f421ae19c109c1500b566ab6c13ddf490f9ac
Arka2BR <80419385+Arka2BR@users.noreply.github.com>
2025-04-02 15:18:37 +0530

    Update metalJogFinder_CDNS.tcl

--- TCL/metalJogFinder_CDNS.tcl
+++ TCL/metalJogFinder_CDNS.tcl
@@ -4,7 +4,7 @@ proc get_jog_count { } {
      foreach i $ALL_Mx_LAYERS { 
            set lyrName [get_db $i .name] ;
            set lyrDir [get_db $i .direction]
-           if { ($lyrDir eq "vertical") && () } {
+           if { ($lyrDir == "vertical") && () } {
                  set  jogs [get_db nets .wires -if {.layer==$i && .direction!=vertical}]
                  puts $jogs
            } else {
