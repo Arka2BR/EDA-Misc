d406bf068e3096a66fc38047616679ed2242bde7
Arka2BR <80419385+Arka2BR@users.noreply.github.com>
2025-04-02 14:17:42 +0530

    Update metalJogFinder_CDNS.tcl

--- TCL/metalJogFinder_CDNS.tcl
+++ TCL/metalJogFinder_CDNS.tcl
@@ -1,14 +1,14 @@
 proc get_jog_count { } {
      #set ALL_Mx_LAYERS [get_layers -filter "layer_type == interconnect && mask_name =~ metal*"]
-     set ALL_Mx_LAYERS [get_db layers -if {.type==routing}]
+     set ALL_Mx_LAYERS [get_db layers -if {.type==routing && .backside==false}]
      puts "Layer Name, No of jogs"
 foreach_in_collection  i $ALL_Mx_LAYERS { 
-      set lyrName [get_attribute $i name] ;
-      set lyrDir [get_attribute $i routing_direction] 
-      if { $lyrDir eq "vertical" } {
-       set  jogs [ get_shapes -quiet -filter "layer.name == $lyrName && is_vertical != true"] 
+      set lyrName [get_db $i .name] ;
+      set lyrDir [get_db $i .direction]
+      if { ($lyrDir eq "vertical") && () } {
+            set  jogs [get_db nets .wires -if {.layer==$i && .direction!=vertical}] 
       } else {
-       set  jogs [ get_shapes -quiet -filter "layer.name == $lyrName && is_horizontal != true"] 
+            set  jogs [ get_db nets .wires -if {.layer==$i && .direction!=horizontal}] 
        }
       puts "[get_attribute $i name], [sizeof_collection $jogs]"                      }
 
