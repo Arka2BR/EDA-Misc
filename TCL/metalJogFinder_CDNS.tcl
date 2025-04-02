9f0415f0b73a25b4a2ddec6e69f86f027fa755b8
Arka2BR <80419385+Arka2BR@users.noreply.github.com>
2025-04-02 15:17:15 +0530

    Update metalJogFinder_CDNS.tcl

--- TCL/metalJogFinder_CDNS.tcl
+++ TCL/metalJogFinder_CDNS.tcl
@@ -1,16 +1,16 @@
 proc get_jog_count { } {
      set ALL_Mx_LAYERS [get_db layers -if {.type==routing && .backside==false}]
      puts "Layer Name, No of jogs"
-foreach_in_collection  i $ALL_Mx_LAYERS { 
-      set lyrName [get_db $i .name] ;
-      set lyrDir [get_db $i .direction]
-      if { ($lyrDir eq "vertical") && () } {
-            set  jogs [get_db nets .wires -if {.layer==$i && .direction!=vertical}]
-            puts $jogs
-      } else {
-            set  jogs [ get_db nets .wires -if {.layer==$i && .direction!=horizontal}]
-            puts $jogs
-       }
-      puts "[get_db $i name], [sizeof_collection $jogs]"
+     for i $ALL_Mx_LAYERS { 
+           set lyrName [get_db $i .name] ;
+           set lyrDir [get_db $i .direction]
+           if { ($lyrDir eq "vertical") && () } {
+                 set  jogs [get_db nets .wires -if {.layer==$i && .direction!=vertical}]
+                 puts $jogs
+           } else {
+                 set  jogs [ get_db nets .wires -if {.layer==$i && .direction!=horizontal}]
+                 puts $jogs
+            }
+           puts "[get_db $i name], [sizeof_collection $jogs]"
       }
 }
