234ca378e08677d2e0c48a50f48787bdf3a66439
Arka2BR <80419385+Arka2BR@users.noreply.github.com>
2025-04-02 15:56:12 +0530

    Update metalJogFinder_CDNS.tcl

--- TCL/metalJogFinder_CDNS.tcl
+++ TCL/metalJogFinder_CDNS.tcl
@@ -5,14 +5,14 @@ proc get_jog_count { } {
            set lyrName [get_db $i .name] ;
            set lyrDir [get_db $i .direction] ;
             set all_nets [get_db nets .wires -if {.layer==$i}] ;
-            set net_count [expr double([llength $all_nets])]
+            set net_count [expr double([llength $all_nets])] ;
            if { ($lyrDir == "vertical")} {
                  set  jogs [get_db nets .wires -if {.layer==$i && .direction!=vertical}] ;
            } else {
                  set  jogs [ get_db nets .wires -if {.layer==$i && .direction!=horizontal}] ;
             }
-           set jog_count [expr double([llength $jogs])]
+           set jog_count [expr double([llength $jogs])] ;
            set float jog_ratio [expr {$jog_count*100/$net_count]];
-           puts "$lyrName, $lyrDir, [llength $all_nets], [llength $jogs], [format %.2f $jog_ratio]"
+           puts "$lyrName, $lyrDir, $net_count, $jog_count, [format %.2f $jog_ratio]"
       }
 }
