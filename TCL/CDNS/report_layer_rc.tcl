9d63a68355b9d0033fe67100950d17fc91fe8c75
Arka2BR <80419385+Arka2BR@users.noreply.github.com>
2025-04-08 13:46:21 +0530

    Update report_layer_rc.tcl

--- TCL/CDNS/report_layer_rc.tcl
+++ TCL/CDNS/report_layer_rc.tcl
@@ -3,10 +3,10 @@
 ##########################################################################
 create_flow_step -name report_layer_rc -owner cadence {
 
-set LAYER_COUNT [llength [get_db layers -if {.type==routing && .backside==false}]]
+set METAL_LAYERS [get_db layers -if {.type==routing && .backside==false}]
 set RC_CORNERS [all_rc_corners]
 set REPORT_FILE rc_report.rpt
-for layer $METAL_LAYERS {
+foreach layer $METAL_LAYERS {
 	foreach corner $RC_CORNERS {
 		report_unit_parasitics -layer $layer -rc_corner $corner >> $REPORT_FILE
 			         }
